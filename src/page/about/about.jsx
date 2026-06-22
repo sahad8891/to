be343865d8c249a626dfec835a502cf821204223
sahad8891 <sahadthadathilakath@gmail.com>
2026-06-22 12:15:39 +0530

    created home page

---
+++ src/page/about/about.jsx
@@ -0,0 +1,10 @@
+function about() {
+    return (
+        <>
+        <h1>about page</h1>
+        <p>Welcome to the about page!</p>
+        </>
+    )
+}
+
+export default about
---
+++ src/page/home/home.jsx
@@ -0,0 +1,10 @@
+function home() {
+    return (
+        <div>
+        <h1>home page</h1>
+        <p>Welcome to the home page!</p>
+        </div>
+    )
+}
+
+export default home
